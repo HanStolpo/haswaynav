65699d95842d9baae01462ae1b455fc9a9b2d165
Handré Stolp <HanStolpo@gmail.com>
2024-01-05 16:12:14 +0100

    add overlay

--- flake.nix
+++ flake.nix
@@ -9,30 +9,36 @@
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          naersk-lib = pkgs.callPackage naersk { };
+        in
+        {
+          packages.default = naersk-lib.buildPackage ./.;
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+          apps.default = utils.lib.mkApp {
+            drv = self.defaultPackage."${system}";
+          };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            rustc
-            rustfmt
-            pre-commit
-            rustPackages.clippy
-            rust-analyzer
-            treefmt
-            nixpkgs-fmt
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              rust-analyzer
+              treefmt
+              nixpkgs-fmt
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+        })
+    // {
+      overlays.default = final: prev: {
+        haswaynav = self.packages.${prev.system}.default;
+      };
+    };
 }
